ebd8794e2f53eeddf9f884fc34e878f3fb926f7b
Vitor <vitordverissimo@gmail.com>
2022-10-02 18:12:43 -0500

    added basic personal info card

--- app/src/main/java/com/example/portfolio/MainActivity.kt
+++ app/src/main/java/com/example/portfolio/MainActivity.kt
@@ -4,25 +4,31 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxHeight
-import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.Card
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
 import com.example.portfolio.ui.theme.PortfolioTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        val padding = resources.getDimension(R.dimen.card_padding)
         setContent {
             PortfolioTheme {
                 Box {
                     Background()
                     Column {
-                        
+                        PersonalInfoCard(padding.dp)
                     }
                 }
                 // A surface container using the 'background' color from the theme
@@ -49,6 +55,29 @@ fun Background() {
             .fillMaxWidth(),
         contentScale = ContentScale.Crop
     )
+}
+
+@Composable
+fun PersonalInfoCard(padding: Dp) {
+    val me = painterResource(R.drawable.me)
+    Card(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(all = padding)
+    ) {
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Image(
+                painter = me,
+                contentDescription = null,
+                modifier = Modifier.clip(CircleShape)
+            )
+            Text(stringResource(R.string.my_name))
+            Text(stringResource(R.string.email))
+            Text(stringResource(R.string.github_link))
+        }
+    }
 }
 
 /*
